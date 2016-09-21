d846d42df0c6a894012179dc33f6acd917227f41
Alexander Lukin <alexander.lukin@acronis.com>
2016-09-21 16:08:21 +0300

    cache book service data

--- src/core/services/books.service.srv.js
+++ src/core/services/books.service.srv.js
@@ -1,16 +1,38 @@
 /* @ngInject */
-export default function BookService($http) {
-
+export default function BookService($http, $q) {
     /**
      * For test purposes only
      */
     const url = '/assets/books.mock.json';
 
     /**
+     * Array of book objects (cache)
+     * @type {Array<Object>}
+     */
+    let books = null;
+
+    /**
+     * Get from cache
+     * @type {Array<Object>}
+     */
+    function getFromCache() {
+        const defer = $q.defer();
+        defer.resolve(books);
+        return defer.promise;
+    }
+
+    /**
      * Fetch all books
      */
-    function fetch() {
-        return $http.get(url);
+    function fetch(clearCache = false) {
+        if (books && !clearCache) {
+            return getFromCache();
+        }
+
+        return $http.get(url).then((response) => {
+            books = response.data; // cache
+            return books;
+        });
     }
 
     return {
