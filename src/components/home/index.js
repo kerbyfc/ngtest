8c24450648a52c64aede34dbb96a2fcdcd0b049d
Alexander Lukin <alexander.lukin@acronis.com>
2016-09-21 10:04:59 +0300

    cleanup home component

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,24 +1,14 @@
 import angular from 'angular';
-import uiRouter from 'angular-ui-router';
-import AppCore from '../../core';
-import { HomeComponent } from './home.component';
+import {HomeComponent} from './home.component';
 
 export * from './home.component';
 
-export default angular.module('home', [
-  AppCore,
-  uiRouter
-])
-  .config(config)
-  .component(HomeComponent.selector, HomeComponent)
-.name;
-  // .config(config);
+export default angular.module('home', [])
+    .config(config)
+    .component(HomeComponent.selector, HomeComponent)
+    .name;
 
 /* @ngInject */
-function config ($stateProvider) {
-  // $stateProvider
-  //     .state('home', {
-  //         url: '/home',
-  //         template: '<other></other>'
-  //     });
+function config() {
+
 }
