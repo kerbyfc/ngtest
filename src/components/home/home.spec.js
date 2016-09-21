c46539edbecac6add434dd66767d15d0022c18d5
Alexander Lukin <alexander.lukin@acronis.com>
2016-09-21 12:21:12 +0300

    fix home page tests

--- src/components/home/home.spec.js
+++ src/components/home/home.spec.js
@@ -1,22 +1,23 @@
 'use strict';
-import HomeModule, { HomeComponent } from './index.js';
+import HomeModule, {HomeComponent} from './index.js';
 // if a mock json object is needed for tests
 // import HomeMock from '../../../tests/mocks/home.mock.json';
 
 describe('Home Component', () => {
-  let ctrl;
+    var ctrl, myScope;
 
-  beforeEach(window.module(HomeModule));
+    beforeEach(window.module(HomeModule));
 
-  beforeEach(window.inject(($componentController) => {
-    ctrl = $componentController(HomeComponent.selector, { 
-      $state: {}
+    beforeEach(window.inject(($componentController, $rootScope) => {
+        myScope = $rootScope.$new();
+        ctrl = $componentController(HomeComponent.selector, {
+            $state: {},
+            $scope: myScope
+        });
+    }));
+
+    it('should have a title', () => {
+        const expected = 'Best 10 Books & Authors';
+        expect(myScope.title).toMatch(expected);
     });
-  }));
-
-  it('should have a title', () => {
-    const expected = 'SuperNova';
-    const actual = ctrl.title;
-    expect(actual).toMatch(expected);
-  });
 });
