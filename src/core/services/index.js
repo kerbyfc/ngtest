4f8ae9b885ba9e8faeb6c3a3e05ae095972e9236
Alexander Lukin <alexander.lukin@acronis.com>
2016-09-21 15:48:14 +0300

    emulate server latency

--- src/core/services/index.js
+++ src/core/services/index.js
@@ -7,5 +7,35 @@ export default angular
     .module('core.services', [
         // 'LocalStorageModule'
     ])
+    .config(['$httpProvider', function ($httpProvider) {
+
+        /**
+         * For test purposes only.
+         */
+        $httpProvider.interceptors.push([
+            '$q', '$timeout',
+            function ($q, $timeout) {
+
+                var delay = Math.random();
+
+                return {
+                    'response': function(response) {
+                        var defer = $q.defer();
+
+                        if (typeof(response.data) !== 'object') {
+                            return response;
+                        }
+
+                        $timeout(function () {
+                            defer.resolve(response);
+                        }, delay * 3000);
+
+                        return defer.promise;
+                    }
+                };
+            }
+        ]);
+
+    }])
     .factory('BooksService', BooksService)
 ;
