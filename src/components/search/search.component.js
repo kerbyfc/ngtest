f01bd423c9249717524d5c9ceb035ac054e5dcb5
Alexander Lukin <alexander.lukin@acronis.com>
2016-09-22 17:12:24 +0300

    add filtering and ordering

--- src/components/search/search.component.js
+++ src/components/search/search.component.js
@@ -10,9 +10,16 @@ export let SearchComponent = {
 
         /* @ngInject */
         constructor($scope, $state, BooksService) {
+            const search = angular.element(document.querySelector('[data-search-input]'));
+
             $scope.loading = true;
             $scope.books = [];
             $scope.q = $state.current.q || '';
+            $scope.sorting = 'rating';
+
+            $scope.sorter = (item) => {
+                return item[$scope.sorting];
+            };
 
             BooksService.fetch()
                 .then((books) => {
@@ -20,6 +27,7 @@ export let SearchComponent = {
                 })
                 .finally(() => {
                     $scope.loading = false;
+                    search.focus()
                 });
         }
     }
