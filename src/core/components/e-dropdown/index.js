3f5fe03665fab05c9d7a25cdc7ec19c2b71b410a
Alexander Lukin <alexander.lukin@acronis.com>
2016-09-22 18:15:43 +0300

    remove e-dropdown

--- src/core/components/e-dropdown/index.js
+++
@@ -1,6 +0,0 @@
-import angular from 'angular';
-import { eDropdown } from './e-dropdown.component';
-
-export default angular.module('eDropdown', [])
-  .component(eDropdown.selector, eDropdown)
-  .name;
